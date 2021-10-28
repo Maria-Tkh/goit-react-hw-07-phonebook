203a11906115c8706bc2874f93d22023af93c53a
Maria-Tkh <mcweaver117@gmail.com>
2021-10-28 17:25:26 +0300

    filter_corr

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,6 @@ const App = () => {
   };
   const visibleContacts = getVisibleContacts();
 
-  // const [createContact] = useCreateContactMutation();
   return (
     <div>
       <PhonebookTitle>Phonebook</PhonebookTitle>
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,16 +2,16 @@ import PropTypes from 'prop-types';
 // import { useState } from 'react';
 import { Label } from './Filter.styled';
 
-const Filter = ({ filter, onChange }) => {
+const Filter = ({ value, onChange }) => {
   //   const [filter, setFilter] = useState('');
-  //   const changeFilter = e => {
-  //     setFilter(e.target.value);
-  //   };
+  // const changeFilter = e => {
+  //   setFilter(e.target.value);
+  // };
 
   return (
     <Label>
       <p> Find contact by name</p>
-      <input type="text" value={filter} onChange={onChange} />
+      <input type="text" value={value} onChange={onChange} />
     </Label>
   );
 };
